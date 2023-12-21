defd7f9a0f4f25ab8e45c8cff11bd19539dc2a7f
jacks03 <kjarisalfaro14@gmail.com>
2023-12-20 22:32:10 -0500

    noveno 20/12/2023

--- food-ordering-app/src/app/register/page.js
+++ food-ordering-app/src/app/register/page.js
@@ -1,7 +1,38 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
 export default function RegisterPage(){
+    const [email,setEmail] = useState('');
+    const [password, setPasword] =useState('');
+    function handleFormSubmit(ev){
+        ev.preventDefault();
+        fetch('/api/register', {
+            method: 'POST',
+            body: JSON.stringify({email,password}),
+            headers: {'Content-Type': 'application/json'},
+        })
+    }
     return (
         <section className="mt-8">
-            <h1 className=" text-center text-primary text-4xl " > Register</h1>
+            <h1 className=" text-center text-primary text-4xl 
+            mb-4" > 
+            Register
+            </h1>
+            <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+                <input type="email" placeholder="email" value={email}
+                        onChange={ev => setEmail(ev.target.value)}/>
+                <input type="password" placeholder="password" value={password}
+                        onChange={ev => setPasword(ev.target.value)}/>
+                <button type="submit">Register </button>
+                <div className="my-4 text-center text-gray-500">
+                    or login width
+                </div>
+                <button className="flex gap-4 justify-center ">
+                    <Image src={'/google.png'} alt={''} width={24} height={24} />
+                    Login width Google 
+                </button>
+            </form>
         </section>
     );
 }
